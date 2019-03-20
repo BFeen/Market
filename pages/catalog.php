c1216a2c64b3cd36ab92e843fc7ee625f07f9d6e
Feen <bfeen@mail.ru>
2019-03-20 16:54:47 +0200

    Some fixes

--- pages/catalog.php
+++ pages/catalog.php
@@ -21,13 +21,13 @@
                 </nav>
             </div>
             <div class="header__item">
-                <div class="login">
+                <div class="login login_margin-right">
                     <div class="login__pic"></div>
                     <p class="inside-text">
                         Привет, Алексей (<span class="inside-text_orange">выйти</span>)
                     </p>
                 </div>
-                <div class="basket">
+                <div class="basket basket_margin-right">
                     <div class="basket__logo"></div>
                     <p class="inside-text">
                         Корзина (5)
@@ -44,82 +44,82 @@
             <form method="GET" class="filter">
                 <select class="filter__item" name="category">
                     <option>Категория</option>
-                    <option name="outerwear">Верхняя одежда</option>
-                    <option name="underwear">Нижняя одежда</option>
-                    <option name="pants">Штаны</option>
+                    <option value="outerwear">Верхняя одежда</option>
+                    <option value="underwear">Нижняя одежда</option>
+                    <option value="pants">Штаны</option>
                 </select>
                 <select class="filter__item" name="size">
                     <option>Размер</option>
-                    <option name="l">L</option>
-                    <option name="m">M</option>
-                    <option name="s">S</option>
+                    <option value="l">L</option>
+                    <option value="m">M</option>
+                    <option value="s">S</option>
                 </select>
                 <select class="filter__item" name="cost">
                     <option>Стоимость</option>
-                    <option name="1000">0 - 1000 руб.</option>
-                    <option name="3000">1000 - 3000 руб.</option>
-                    <option name="6000">3000 - 6000 руб.</option>
-                    <option name="20000">6000 - 20000 руб.</option>
+                    <option value="1000">0 - 1000 руб.</option>
+                    <option value="3000">1000 - 3000 руб.</option>
+                    <option value="6000">3000 - 6000 руб.</option>
+                    <option value="20000">6000 - 20000 руб.</option>
                 </select>
             </form>
             <section class="section">
-                <div class="card">
-                    <div class="card__item">
+                <div class="catalog">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
                     </div>
-                    <div class="card__item">
+                    <div class="card">
                         <div class="card__pic"></div>
                         <div class="card__title">Наименование товара</div>
                         <div class="card__cost">1000 руб.</div>
@@ -152,10 +152,10 @@
                 <h3 class="footer__title">Мы в социальных сетях</h3>
                 <p class="footer__text">Сайт разработан в inordic.ru</p>
                 <p class="footer__text">2018 &copy; Все права защищены</p>
-                <div class="footer__icons">
-                    <a href="#" class="icon tw"></a>
-                    <a href="#" class="icon fb"></a>
-                    <a href="#" class="icon inst"></a>
+                <div class="social">
+                    <a href="#" class="social__icon social__icon_margin-right tw"></a>
+                    <a href="#" class="social__icon social__icon_margin-right fb"></a>
+                    <a href="#" class="social__icon social__icon_margin-right inst"></a>
                 </div>
             </div>
         </footer>
